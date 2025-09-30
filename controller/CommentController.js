a0100ddcdbfc35be75531df3b3366b1fcadb7700
Krishnatripathi09 <192.168.9.99kj@gmail.com>
2025-09-30 21:36:37 +0530

    feat(comment): Created comment API's to create,read and delete comments

---
+++ controller/CommentController.js
@@ -0,0 +1,52 @@
+import prisma from "../DB/db.config.js";
+
+export const createComment = async (req, res) => {
+  const { user_id, title, description } = req.body;
+
+  if (!user_id || !title || !description) {
+    res.status(400).send(" All the Fields are required");
+  }
+
+  const newComment = await prisma.comment.create({
+    data: {
+      user_id: Number(user_id),
+      post_id_id: Number(post_id),
+      comment,
+    },
+  });
+
+  return res.json({
+    status: 201,
+    data: newComment,
+    msg: "Comment created Successfully",
+  });
+};
+
+export const fetchComments = async (req, res) => {
+  const commentId = req.params.id;
+  const comment = await prisma.comment.findFirst({
+    where: {
+      id: Number(commentId),
+    },
+  });
+
+  return res.json({ status: 200, data: comment });
+};
+
+export const fetchAllPosts = async (req, res) => {
+  const posts = await prisma.comment.findMany({});
+
+  return res.json({ status: 200, data: posts });
+};
+
+export const deletePost = async (req, res) => {
+  const postId = req.params.id;
+
+  await prisma.comment.delete({
+    where: {
+      id: Number(postId),
+    },
+  });
+
+  return res.json({ status: 200, msg: "Comment Deleted Successfully" });
+};
